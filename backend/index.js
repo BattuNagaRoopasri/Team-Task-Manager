f3f59c69bf7d1011e70615d887994a68ee5a2938
Rupa <roopasri.0812@gmail.com>
2026-05-09 21:08:20 +0530

    fix CORS origin issues

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'https://teamtaskmanagerss.netlify.app',
+  origin: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
